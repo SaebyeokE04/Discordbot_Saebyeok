d399df630fdbd5bd62305e9d2a9c2a3b3ac8badd
SaebyeokE04 <154464919+SaebyeokE04@users.noreply.github.com>
2023-12-22 21:23:11 +0900

    Add files via upload
    현재 상황
    인사 기능(안녕으로 시작하는 문장)
    강아지 고양이 사진 기능(!강아지, !고양이)
    현재 시간 (!시간)
    청소 기능(!청소 청소할 개수)
    날씨 기능: 다른 API를 찾아봐야 할 것 같아요.. 저거 현재 날씨 부분에서 'clear'만 뽑아오고 싶은데 쟤는 안 뽑힘

--- bo.py
+++ bo.py
@@ -1,5 +1,6 @@
 import discord
 import requests
+from datetime import datetime
 from discord.ext import commands
 from disco_token import Token
 
@@ -12,7 +13,7 @@ bot = commands.Bot(intents=intents, command_prefix='!')
 @bot.event
 async def on_ready():
     print(f"봇이 성공적으로 로그인 되었습니다. 봇 이름 : {bot.user.name}")
-
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('새벽 감성 타기'))
 
 @bot.event
 async def on_message(message):
@@ -23,10 +24,6 @@ async def on_message(message):
         await message.channel.send('안녕하세요!')
     
     await bot.process_commands(message)
-
-@bot.command()
-async def 핑(ctx):
-    await ctx.send('pong')
 
 @bot.command(name="고양이")
 async def 고양이(ctx):
@@ -49,5 +46,27 @@ async def 청소(ctx, amount: int):
         await ctx.send(f"{amount}개의 메시지가 삭제되었습니다.", delete_after=5)
     else:
         await ctx.send("한 번에 최대 99개의 메세지를 삭제할 수 있어요!")
+
+@bot.command(name="시간")
+async def 시간(ctx):
+    now = datetime.now()
+    current_time = now.strftime("%Y년 %m월 %d일 %H시 %M분 %S초")
+    await ctx.send(f"현재 시간은 {current_time}입니다.")
+
+@bot.command(name="날씨")
+async def 날씨(ctx):
+    locdata = requests.get("https://ipinfo.io/")
+    location = locdata.json()
+    loc = location["city"]
+    api_key = "25fd02ed63bdc22ad6f08381eeda43fd"  # OpenWeatherMap API 키를 입력하세요.
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={loc}&appid={api_key}&units=metric"
+    responce = requests.get(url)
+    data = responce.json()
+    temperature = data["main"]["temp"]
+    temp_feel = data["main"]["feels_like"]
+    weather = data["weather"]
+    print(weather)
+    
+    await ctx.send(f"```\n\n현재 지역 : {loc}\n\n현재 온도 : {temperature} °C\n\n체감 온도 : {temp_feel} °C\n\n현재 날씨 : {weather} \n\n```")
 
 bot.run(Token)
